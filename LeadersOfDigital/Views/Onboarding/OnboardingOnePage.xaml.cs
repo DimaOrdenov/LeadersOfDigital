03892b894c807e5bef3e44617be76b8462d3098a
Dima Ordenov <dima.ordenov@gmail.com>
2020-10-18 02:11:51 +0300

    fix

--- LeadersOfDigital/Views/Onboarding/OnboardingOnePage.xaml.cs
+++ LeadersOfDigital/Views/Onboarding/OnboardingOnePage.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using NoTryCatch.Xamarin.Portable.ViewControls;
 using Xamarin.Forms;
 
 namespace LeadersOfDigital.Views.Onboarding
 {
-    public partial class OnboardingOnePage : ContentPage
+    public partial class OnboardingOnePage : BasePage
     {
         public OnboardingOnePage()
         {
